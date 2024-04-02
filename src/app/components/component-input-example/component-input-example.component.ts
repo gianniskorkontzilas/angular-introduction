966dcfa34e696a46ddf683a9f770c7c1c6e42c87
“[giannis <gianniskorkontzilas@hotmail.com>
2024-04-02 13:50:33 +0300

    routing primer finished

---
+++ src/app/components/component-input-example/component-input-example.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
+import { PersonTableComponent } from "../person-table/person-table.component";
+
+@Component({
+    selector: 'app-component-input-example',
+    standalone: true,
+    templateUrl: './component-input-example.component.html',
+    styleUrl: './component-input-example.component.css',
+    imports: [PersonTableComponent]
+})
+export class ComponentInputExampleComponent {
+  person0: Person = {
+    givenName: 'Giannis',
+    surName: 'Korkontzilas',
+    age: 0,
+    email: 'g@aueb.gr',
+    address:'Athens, Greece',
+  }
+
+  person1: Person = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 0x25,
+    email: 'john@example.com',
+    address: 'New York, USA',
+  };
+}
