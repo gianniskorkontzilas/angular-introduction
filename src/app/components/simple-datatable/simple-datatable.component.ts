88a645903c8dfb086a34907341af3498de1d89ce
“[giannis <gianniskorkontzilas@hotmail.com>
2024-04-08 14:17:15 +0300

    simple datatable

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { EPerson } from 'src/app/shared/interfaces/person';
+import { sortBy } from 'lodash-es';
 
 @Component({
   selector: 'app-simple-datatable',
@@ -11,4 +12,37 @@ import { EPerson } from 'src/app/shared/interfaces/person';
 export class SimpleDatatableComponent {
 @Input() data: EPerson[];
  
+sortOreder = {
+  givenName: 'none',
+  surName: 'none',
+  age: 'none',
+  email: 'none',
+  education: 'none',
+}
+
+sortData(sortKey: string) {
+  if (this.sortOreder[sortKey] === 'asc') {
+    this.sortOreder[sortKey] = 'desc';
+    this.data = sortBy(this.data, sortKey).reverse();
+  } else {
+    this.sortOreder[sortKey] = 'asc';
+    this.data = sortBy(this.data, sortKey);
+  }
+
+  for (let key in this.sortOreder) {
+    if (key !== sortKey) {
+      this.sortOreder[key] = 'none';
+    }
+  }
+}
+
+sortSign(sortKey: string) {
+  if (this.sortOrder[sortKey] === 'asc') {
+    return '↑';
+  } else if (this.sortOrder[sortKey] === 'desc') {
+    return '↓';
+  } else {
+    return '';
+  }
+ }
 }
